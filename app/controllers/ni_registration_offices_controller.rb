2ef784187086da1d523f283fbdb378f0bfd45443
David Basalla <davidbasalla@gmail.com>
2016-01-28 16:58:23 +0000

    Add sorting for NI

--- app/controllers/ni_registration_offices_controller.rb
+++ app/controllers/ni_registration_offices_controller.rb
@@ -1,5 +1,17 @@
 class NiRegistrationOfficesController < ApplicationController
+  helper_method :sort_column, :sort_direction
+  
   def index
-    @mappings = NiRegistrationOffice.all.order(:council)
+    @mappings = NiRegistrationOffice.all.order(sort_column + " " + sort_direction)
+  end
+
+  private
+
+  def sort_column
+    NiRegistrationOffice.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
