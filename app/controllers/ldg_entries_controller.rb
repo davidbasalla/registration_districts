f62523e7b65aa4b80093517d8c5e4e8486fb5f69
David Basalla <davidbasalla@gmail.com>
2016-01-28 17:40:26 +0000

    Add sorting to LDG

--- app/controllers/ldg_entries_controller.rb
+++ app/controllers/ldg_entries_controller.rb
@@ -1,5 +1,17 @@
 class LdgEntriesController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
-    @entries = LdgEntry.all.order(:title)
+    @entries = LdgEntry.all.order(sort_column + " " + sort_direction)
+  end
+
+  private
+
+  def sort_column
+    LdgEntry.column_names.include?(params[:sort]) ? params[:sort] : "title"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
