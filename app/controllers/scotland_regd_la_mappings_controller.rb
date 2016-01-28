7113412f026f9e7138a55f4e573454f29a241ce7
David Basalla <davidbasalla@gmail.com>
2016-01-28 17:05:46 +0000

    Add sorting for Scotland index page

--- app/controllers/scotland_regd_la_mappings_controller.rb
+++ app/controllers/scotland_regd_la_mappings_controller.rb
@@ -1,5 +1,15 @@
 class ScotlandRegdLaMappingsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
-    @mappings = ScottishRegistrationDistrict.all.order(:council)
+    @mappings = ScottishRegistrationDistrict.all.order(sort_column + " " + sort_direction)
+  end
+
+  def sort_column
+    ScottishRegistrationDistrict.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
